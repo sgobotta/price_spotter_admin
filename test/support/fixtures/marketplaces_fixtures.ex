fd6f9ba274d1391ca620ad51e22464dfe5f0a926
Santiago Botta <sgobotta@gmail.com>
2023-07-21 01:22:03 -0300

    Add marketplaces fixture attrs

--- test/support/fixtures/marketplaces_fixtures.ex
+++ test/support/fixtures/marketplaces_fixtures.ex
@@ -4,21 +4,36 @@ defmodule PriceSpotter.MarketplacesFixtures do
   entities via the `PriceSpotter.Marketplaces` context.
   """
 
+  @valid_attrs %{
+    category: "some category",
+    img_url: "some img_url",
+    internal_id: "some internal_id",
+    name: "some name",
+    price: "120.5",
+    supplier_name: "some supplier_name",
+    supplier_url: "some supplier_url"
+  }
+
+  @update_attrs %{
+    category: "some updated category",
+    img_url: "some updated img_url",
+    internal_id: "some updated internal_id",
+    name: "some updated name",
+    price: "241.0",
+    supplier_name: "some updated supplier_name",
+    supplier_url: "some updated supplier_url"
+  }
+
+  def valid_attrs(attrs \\ %{}), do: Enum.into(attrs, @valid_attrs)
+  def update_attrs(attrs \\ %{}), do: Enum.into(attrs, @update_attrs)
+
   @doc """
   Generate a product.
   """
   def product_fixture(attrs \\ %{}) do
     {:ok, product} =
       attrs
-      |> Enum.into(%{
-        category: "some category",
-        img_url: "some img_url",
-        internal_id: "some internal_id",
-        name: "some name",
-        price: "120.5",
-        supplier_name: "some supplier_name",
-        supplier_url: "some supplier_url"
-      })
+      |> Enum.into(@valid_attrs)
       |> PriceSpotter.Marketplaces.create_product()
 
     product
