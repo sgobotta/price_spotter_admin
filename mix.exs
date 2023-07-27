285b9df0d3dbd5125d519673ef8b58ed9304390a
Santiago Botta <sgobotta@gmail.com>
2023-07-27 03:42:02 -0300

    Bump minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PriceSpotter.MixProject do
   def project do
     [
       app: :price_spotter,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
