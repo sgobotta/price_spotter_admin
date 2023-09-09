5f4b378f892e68dd4ed3bbce5abff4830158be2b
Santiago Botta <sgobotta@gmail.com>
2023-09-09 17:43:18 -0300

    Add missing mix tasks

--- mix.exs
+++ mix.exs
@@ -72,6 +72,8 @@ defmodule PriceSpotter.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      "setup.web": ["assets.deploy"],
+      "check.format": ["format --check-formatted"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
