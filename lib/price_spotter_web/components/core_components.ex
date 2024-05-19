b6e979b591f11ee6554c2992837017eceffdab59
Santiago <sgobotta@gmail.com>
2024-05-19 20:04:45 -0300

    Update form background color

--- lib/price_spotter_web/components/core_components.ex
+++ lib/price_spotter_web/components/core_components.ex
@@ -244,7 +244,7 @@ defmodule PriceSpotterWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="mt-10 space-y-8 bg-gray-100">
+      <div class="mt-10 space-y-8">
         <%= render_slot(@inner_block, f) %>
         <div
           :for={action <- @actions}
