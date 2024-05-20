d661e066ec3b1eb5ad6996a026f6b942b1964083
Santiago <sgobotta@gmail.com>
2024-05-20 01:57:21 -0300

    Updates copy hook to handle Enter keydown

--- assets/js/hooks/copy.js
+++ assets/js/hooks/copy.js
@@ -1,10 +1,21 @@
 export default {
   mounted() {
     let { to } = this.el.dataset;
-    this.el.addEventListener("click", (ev) => {
-      ev.preventDefault();
+
+    const handleClick = (event) => {
+      event.preventDefault();
       let text = document.getElementById(to).value
       navigator.clipboard.writeText(text).then(() => {})
+    }
+
+    this.el.addEventListener("click", (event) => {
+      handleClick(event)
     });
+
+    this.el.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        handleClick(event)
+      }
+    })
   },
 }
