2c024c55001d637351c4d7a6ac930a12f5300c03
Santiago Botta <sgobotta@gmail.com>
2026-09-12 02:52:29 -0300

    Fix format

--- test/price_spotter_web/live/admin/marketplaces/product_live_test.exs
+++ test/price_spotter_web/live/admin/marketplaces/product_live_test.exs
@@ -308,7 +308,9 @@ defmodule PriceSpotterWeb.Admin.Marketplaces.ProductLiveTest do
       assert html =~ product.category
       assert html =~ "sticky top-0"
       assert html =~ "overflow-x-clip overscroll-none px-4 pb-2"
-      refute html =~ "overflow-x-clip overscroll-none px-4 pb-2 sm:px-6 lg:px-8 pt-2"
+
+      refute html =~
+               "overflow-x-clip overscroll-none px-4 pb-2 sm:px-6 lg:px-8 pt-2"
 
       edit_html = show_live |> element("#edit-button") |> render()
       assert edit_html =~ "hero-pencil-solid"
